22cd4159d9d6e194ae03a4061dc06dbc5ef52995
Uwe Schmitt <schmittu@ethz.ch>
2017-05-15 13:10:24 +0200

    consider submitting host in bioperdb.py

--- appliapps/openbis/biopersdb.py
+++ appliapps/openbis/biopersdb.py
@@ -4,6 +4,7 @@ from applicake.app import WrappedApp
 from applicake.apputils import validation
 from applicake.coreutils.arguments import Argument
 from applicake.coreutils.keys import Keys,KeyHelp
+from applicake.coreutils.submitter import name_of_submitting_host
 
 
 class BioPersonalDB(WrappedApp):
@@ -16,11 +17,29 @@ class BioPersonalDB(WrappedApp):
         ]
 
     def prepare_run(self, log, info):
+
+        # For backward compatibility, by default do not provide explicit OpenBIS
+        # instance URL
+        openbis_alt_instances = {
+                #'euler-portal.ethz.ch': 'https://ra-openbis.ethz.ch',
+                'eulertest-portal.ethz.ch': 'https://openbis-test.ethz.ch'
+        }
+
+        submitter = name_of_submitting_host()
+
+        log.info("this is %s" % os.path.abspath(__file__))
+        if submitter is None:
+            submitter = "eulertest-portal.ethz.ch"
+        log.info("submitter is %s" % submitter)
+
+        openbis_instance = openbis_alt_instances.get(submitter)
+        openbis_instance_opt = '' if openbis_instance is None else '-H %s' % openbis_instance
+
         if info["DB_SOURCE"] == "BioDB":
             command = "true"
         elif info["DB_SOURCE"] == "PersonalDB":
             self.rfile = os.path.join(info[Keys.WORKDIR],"getdataset.out")
-            command = "getdataset -v -r %s -o %s %s" % (self.rfile, info["DATASET_DIR"], info["DBASE"])
+            command = "getdataset %s -v -r %s -o %s %s" % (openbis_instance_opt, self.rfile, info["DATASET_DIR"], info["DBASE"])
         else:
             raise RuntimeError("Unkwnown DB_SOURCE " + info["DB_SOURCE"])
         return info, command
